f30272b8fffd05d4a3103190356a5cd9d2f9eac1
jhetherly <jwh0118@me.com>
2014-05-26 07:38:00 -0500

    switched to production mode

--- tests/SampleAnalysis/Compile.sh
+++ tests/SampleAnalysis/Compile.sh
@@ -1,3 +1,4 @@
 # source Compile.sh
 #root -l -q -b 'CompileAnalysis.C("../../../", "MyAnalysis.cxx")'
-root -l -q -b 'CompileAnalysis.C("/Users/jhetherly/src/HAL-ROOT", "MyAnalysis.cxx", "", "", "", "", "", "", "", "", "", true)'
+#root -l -q -b 'CompileAnalysis.C("/Users/jhetherly/src/HAL-ROOT", "MyAnalysis.cxx", "", "", "", "", "", "", "", "", "", true)'
+root -l -q -b 'CompileAnalysis.C("/Users/jhetherly/src/HAL-ROOT", "MyAnalysis.cxx")'
